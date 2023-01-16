4b79a607fa881ae4f04c3d2340d7363427205f50
abhiputgov <abhipg@outlook.com>
2023-01-16 23:26:35 +0530

    http module commit

---
+++ 9_HttpModules.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end(`Welcome to the default home page`);
+    }
+    else if(req.url === '/about'){
+        res.end(`Welcome to the about page`);
+    }else{
+    res.end(`
+    <html>
+    <h>Oops!</h>
+    <p>We do not have the page you seek yet. Come back later, for we might have it.</p>
+    <a href='/'>Back to Home</a>
+    </html>`);
+    }
+});
+
+server.listen(3000);
