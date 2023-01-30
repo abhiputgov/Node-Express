27c5be7b6fcb304497d9db81b8868b36724dfd4c
abhiputgov <abhipg@outlook.com>
2023-01-30 13:26:10 +0530

    refactor scafolding

---
+++ 11_promisifyFSMethods.js
@@ -0,0 +1,14 @@
+//util file to alternative to the default promise wrapper
+const { readFile, writeFile } = require('fs').promises;
+
+const start = async() => {
+    try {
+        const firstText = await readFile('./content/first.txt', 'utf-8');
+        const secondText = await readFile('./content/second.txt', 'utf-8');
+        await writeFile('./content/promisifyCompleteText.txt', `This is promisifed with ${firstText} and ${secondText}`, {flag: 'a'});
+        console.log(firstText,secondText);
+    }
+    catch (err) {console.log(err);}
+};
+
+start();
