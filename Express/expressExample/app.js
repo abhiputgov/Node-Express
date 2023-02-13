a1d02846b637259d1cdc31b0b33865bc806d0a93
abhiputgov <abhipg@outlook.com>
2023-02-13 23:17:35 +0530

    Express recreation of the serving of static files

---
+++ Express/expressExample/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+//app.use
+//app.use is used to setup static files and middlewares for the application
+app.use(express.static(path.join(__dirname, './public')));
+//app.get
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/about', (req, res) => {
+  res.status(200).send('ABOUT PAGE');
+});
+//app.post
+//app.put
+//app.delete
+//app.all
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>RESOURCE NOT FOUND</h1>');
+});
+
+//app.listen()
+app.listen(5000, () => {
+  console.log(`server is listening on port 5000`);
+});
