012e6531fd54f9b9fc674ecddd479f475517072c
abhiputgov <abhipg@outlook.com>
2023-02-22 00:50:30 +0530

    PUT and DELTE methods

--- Express/expressExample/app_callMethods.js
+++ Express/expressExample/app_callMethods.js
@@ -35,6 +35,39 @@ app.post('/api/postmanTest/people', (req, res) => {
   });
 });
 
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const person = people.find((person) => {
+    return person.id === Number(id);
+  });
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: 'Such a person does not exist!' });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id),
+  );
+  return res.status(200).json({ success: true, data: newPeople });
+});
+
 app.post('/login', (req, res) => {
   console.log(req.body);
   res.send('POST request successful');
