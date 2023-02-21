52d62b5f3e24b0c9d692c6f4b90c549358904b24
abhiputgov <abhipg@outlook.com>
2023-02-21 23:58:17 +0530

    POST method in express along with an intro to axios (check it out)

--- Express/expressExample/app_callMethods.js
+++ Express/expressExample/app_callMethods.js
@@ -6,10 +6,35 @@ let { people } = require('./databaseRepl/people');
 
 app.use(express.static(path.join(__dirname, './methods-public')));
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.json());
 app.get('/api/people', (req, res) => {
   res.status(200).json({ data: people, success: true });
 });
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'Please provide a name!' });
+  }
+  res.status(201).json({ person: name, success: true });
+});
+
+app.post('/api/postmanTest/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'Please provide a name for postman test!' });
+  }
+  people = [...people, { id: people.length + 1, name: name }];
+  res.status(201).json({
+    data: [...people],
+    success: true,
+  });
+});
+
 app.post('/login', (req, res) => {
   console.log(req.body);
   res.send('POST request successful');
