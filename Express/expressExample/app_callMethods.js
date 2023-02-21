3877675bda125addad12b1384becdd691cccfb83
abhiputgov <abhipg@outlook.com>
2023-02-21 22:35:28 +0530

    url encoding to app.use

--- Express/expressExample/app_callMethods.js
+++ Express/expressExample/app_callMethods.js
@@ -5,15 +5,15 @@ const PORT = 5000;
 let { people } = require('./databaseRepl/people');
 
 app.use(express.static(path.join(__dirname, './methods-public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/api/people', (req, res) => {
   res.status(200).json({ data: people, success: true });
 });
-
 app.post('/login', (req, res) => {
-  res.send('POSTED');
+  console.log(req.body);
+  res.send('POST request successful');
 });
-
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
