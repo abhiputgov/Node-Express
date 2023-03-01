a338a923f45a870a3972d3c4b699e3e78c6bc1f5
abhiputgov <abhipg@outlook.com>
2023-03-01 19:55:50 +0530

    adding middleware

--- Project Task Manager/starter/app.js
+++ Project Task Manager/starter/app.js
@@ -4,19 +4,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const tasksRoutes = require('./routes/tasks');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-Handler');
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
 app.use('/api/v1/tasks', tasksRoutes);
-
-/*
-app.get('/api/v1/tasks') --> get all tasks
-app.post('/api/v1/tasks') --> create a new task
-app.get('/api/v1/tasks/:id') --> get a task
-app.patch('/api/v1/tasks/:id') --> update a single task
-app.delete('/api/v1/tasks/:id') --> delete a task
-*/
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
