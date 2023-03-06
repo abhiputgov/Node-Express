c45b426dd5609f341d72a957c23419040c45b418
abhiputgov <abhipg@outlook.com>
2023-03-06 15:55:16 +0530

    async wrapper arounf the controller methods

--- Project Task Manager/starter/controllers/tasks.js
+++ Project Task Manager/starter/controllers/tasks.js
@@ -1,67 +1,53 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/AsyncWrapper');
 
-const getTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (err) {
-    res.status(500).json({ msg: err });
+const getTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
+
+const createTasks = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
+
+const getTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.find({ _id: taskID });
+  if (!task) {
+    return res
+      .status(404)
+      .json({ msg: `There is no task with the id ${taskID}` });
   }
-};
-const createTasks = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (err) {
-    res.status(500).json({ msg: err });
+  res.status(200).json({ task });
+});
+
+const patchTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!task) {
+    return res
+      .status(404)
+      .json({ msg: `There is no task with the id ${taskID}` });
   }
-};
-const getTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.find({ _id: taskID });
-    if (!task) {
-      return res
-        .status(404)
-        .json({ msg: `There is no task with the id ${taskID}` });
-    }
-    res.status(200).json({ task });
-  } catch (err) {
-    res.status(500).json({ msg: err });
+  res.status(200).json({ task });
+});
+
+const deleteTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
+  const task = await Task.findOneAndDelete({ _id: taskID });
+  if (!task) {
+    return res
+      .status(404)
+      .json({ msg: `There is no task with the id ${taskID}` });
   }
-};
-const patchTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!task) {
-      return res
-        .status(404)
-        .json({ msg: `There is no task with the id ${taskID}` });
-    }
-    res.status(200).json({ task });
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  }
-};
-const deleteTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndDelete({ _id: taskID });
-    if (!task) {
-      return res
-        .status(404)
-        .json({ msg: `There is no task with the id ${taskID}` });
-    }
-    //res.status(200).json({ task });
-    res.status(200).send();
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  }
-};
+  //res.status(200).json({ task });
+  res.status(200).send();
+});
+
 module.exports = {
   getTasks,
   createTasks,
--- Project Task Manager/starter/db/connect.js
+++ Project Task Manager/starter/db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-const connection_uri =
-  'mongodb+srv://abhipg:mongodb@cluster0.owffkya.mongodb.net/TaskManager';
-
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useUnifiedTopology: true,
