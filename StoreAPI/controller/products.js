da3238e23f3a55237cf7ddbb369210969c7118f7
abhiputgov <abhipg@outlook.com>
2023-03-29 11:03:23 +0530

    upgrade on mongoose

--- StoreAPI/controller/products.js
+++ StoreAPI/controller/products.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
-const product = require('../controller/products');
+const Products = require('../models/products');
 
 const getProducts = async (req, res, next) => {
-  const products = await product.findAll({});
-  if (!products) {
+  const Product = await Products.findAll({});
+  if (!Product) {
     throw new Error('There are no products');
   }
 
   return res.status(200).json({
     msg: 'Products found at last',
+    data: data,
   });
 };
 
