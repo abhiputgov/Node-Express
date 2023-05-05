74043b0ea881d7e48b45df08e8cddd63b2a52dff
abhiputgov <abhipg@outlook.com>
2023-05-05 15:47:33 +0530

    small fix to display the products fetched

--- StoreAPI/controller/products.js
+++ StoreAPI/controller/products.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Products = require('../models/products');
 
 const getProducts = async (req, res, next) => {
-  const Product = await Products.findAll({});
+  const Product = await Products.find({});
   if (!Product) {
     throw new Error('There are no products');
   }
 
   return res.status(200).json({
     msg: 'Products found at last',
-    data: data,
+    data: Product,
   });
 };
 
